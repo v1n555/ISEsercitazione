7c1194552ca5938e35d1e8062a11de4d315322b2
vins <vinss_2001@libero.it>
2023-09-28 12:33:37 +0200

    Sottrazione Divisione

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,13 +2,29 @@ import java.util.stream.DoubleStream;
 
 public class Calculator {
 
-    static double add(double... operands) {
-        return DoubleStream.of(operands)
-                .sum();
+    private int a, b;
+
+    public Calculator() {
+        this.a = 0;
+        this.b = 0;
     }
 
-    static double multiply(double... operands) {
-        return DoubleStream.of(operands)
-                .reduce(1, (a, b) -> a * b);
+    public int somma(int a, int b) {
+        return a + b;
+    }
+
+    public int moltiplicazione(int a, int b) {
+        return a * b;
+    }
+
+    public int sottrazione(int a, int b) {
+        return a - b;
+    }
+
+    public int divisione(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        }
+        else return 0;
     }
 }
