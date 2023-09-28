f9fb447a169e9e7228fcff9cd6f1328c7931958e
Pascareddum <alessandro.pascarella@gmail.com>
2023-09-28 12:46:02 +0200

    sottrazione_divisione

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,7 @@ import java.util.stream.DoubleStream;
 
 public class Calculator {
 
+
     static double add(double... operands) {
         return DoubleStream.of(operands)
                 .sum();
@@ -11,4 +12,14 @@ public class Calculator {
         return DoubleStream.of(operands)
                 .reduce(1, (a, b) -> a * b);
     }
+
+    public int sottrazione(int a,int b){
+        return a-b;
+    }
+    public int divisione(int a,int b){
+        if(b!=0)
+        return a/b;
+        else return 0;
+    }
+    private int a,b;
 }
